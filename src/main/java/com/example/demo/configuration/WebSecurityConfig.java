20f3aee4e9efe52b9da339b0117b850459165806
wangyi160 <wangyi160@hotmail.com>
2021-11-20 21:53:08 +0800

    add roles

--- src/main/java/com/example/demo/configuration/WebSecurityConfig.java
+++ src/main/java/com/example/demo/configuration/WebSecurityConfig.java
@@ -53,12 +53,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		// We don't need CSRF for this example
 		httpSecurity.csrf().disable()
 				// dont authenticate this particular request
-				.authorizeRequests().antMatchers("/authenticate").permitAll().
+				.authorizeRequests()
+				.antMatchers("/authenticate").permitAll()
+				.antMatchers("/hello/admin").hasRole("ADMIN")
+				.antMatchers("/hello/user").hasRole("USER")
 				// all other requests need to be authenticated
-				anyRequest().authenticated().and().
+				.anyRequest().authenticated().and()
+				
 				// make sure we use stateless session; session won't be used to
 				// store user's state.
-				exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+				.exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
 		// Add a filter to validate the tokens with every request
--- src/main/java/com/example/demo/service/JwtUserDetailsService.java
+++ src/main/java/com/example/demo/service/JwtUserDetailsService.java
@@ -2,22 +2,53 @@ package com.example.demo.service;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		if ("javainuse".equals(username)) {
-			return new User("javainuse", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
-					new ArrayList<>());
-		} else {
+			UserDetails user1 = User
+					.withUsername("javainuse")
+					.authorities("ROLE_USER")
+					.passwordEncoder(passwordEncoder::encode)
+					.password("password")
+					.build();
+			
+			return user1;
+			
+//			return new User("javainuse", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+//					new ArrayList<>());
+		}
+		else if ("javaadmin".equals(username)) {
+			UserDetails user1 = User
+					.withUsername("javaadmin")
+					.authorities("ROLE_USER", "ROLE_ADMIN")
+					.passwordEncoder(passwordEncoder::encode)
+					.password("password")
+					.build();
+			
+			return user1;
+			
+//			return new User("javainuse", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+//					new ArrayList<>());
+		}
+		else {
+			System.out.println(username);
 			throw new UsernameNotFoundException("User not found with username: " + username);
 		}
+		
+		
 	}
 }
